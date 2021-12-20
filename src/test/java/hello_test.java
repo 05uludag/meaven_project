14f96e80a9276c2dd8c93db4e53d624e5e77758f
05uludag <kultiginuludag@gmail.com>
2021-12-20 16:10:50 +0300

    commit first2

--- src/test/java/hello_test.java
+++ src/test/java/hello_test.java
@@ -1,5 +1,6 @@
 public class hello_test {
     public static void main(String[] args) {
         System.out.println("hello printing under test");
+        System.out.println("after shared than again");
     }
 }
